1c6915c90397b24ba0d707692235aef3ab289382
mingUsePy <1442434389@qq.com>
2025-08-15 18:58:45 +0800

    Add files via upload

---
+++ Shell.h
@@ -0,0 +1,26 @@
+#include "Video.h"
+
+#pragma pack(1)
+
+typedef struct
+{
+    UINT32 Width;
+    UINT32 Height;
+    UINT32 X;
+    UINT32 Y;
+    UINT32 OutLines;
+} SETUP;
+
+typedef struct
+{
+    SETUP Setup;
+} BASE;
+
+
+#pragma pack()
+
+int ShellInitial();
+int SetCursor(UINT8 Count);
+int PrintHex(UINT64 Number);
+int PrintDec(UINT64 Number);
+int PrintStr(char *String);
